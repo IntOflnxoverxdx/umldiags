195ccc27787a2a0a58e457c6735b34f9b8de3db0
IntOflnxoverxdx <106513380+IntOflnxoverxdx@users.noreply.github.com>
2024-06-22 22:14:08 +1000

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,16 @@
 slide = 0
 
 
-document.querySelectorAll(".controls__item").forEach((e,i) => {
-    e.onclick = ()=>{
-        slide = i
-        setSlides()
-    }
-});
+document.querySelector("#prev").onclick = ()=>{
+    slide -= 1
+    if (slide == -1) slide = 13 
+    setSlides()
+}
+document.querySelector("#next").onclick = ()=>{
+    slide += 1
+    if (slide == 14) slide = 0 
+    setSlides()
+}
 
 function setSlides(){
     document.querySelectorAll(".slider__item").forEach((e,i) => {
